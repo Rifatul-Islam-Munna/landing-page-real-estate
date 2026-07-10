a2eb2152e20466ea0d46e1c1d60b37690ff332f8
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-10 16:05:27 +0600

    Mount Motion animation effects

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import MotionEffects from "./MotionEffects";
 import ScreenshotSliderEnhancer from "./ScreenshotSliderEnhancer";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         {children}
         <ScreenshotSliderEnhancer />
+        <MotionEffects />
       </body>
     </html>
   );
